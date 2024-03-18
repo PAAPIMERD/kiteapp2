0bb4e1460c678a718ee1f9456640b196ea825d1f
PAAPIMERD <97976194+PAAPIMERD@users.noreply.github.com>
2024-03-18 22:00:57 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+#FINAL PRODUCTION LEVEL TO BE CALLED 
+
+#import all the neccessary librarys
+import kiteapp as kt
+import datetime
+import time
+
+
+#all the code required to authentication of kotak neo and kite api should be done here
+
+enc = "cDgt17d1ENTb2renX2kE13mtRuALDqqfSB4E9vSQrDgLFVl/HeP5Wjl6syBapi/glnH5lCzjmS0da5/tFGBYm293HGDn9Oy1Dx/WeVnxR5YfA94Te2QuJw==" #login into kite goto ->inspect->networks->full->enctoken
+kite = kt.KiteApp(enc)#this block will authenticate the user using enc token in kite(zerodha)
+
+#below you can write code for the authentication of kotakapi (which will be used for order placing purpose)
+
+
+
+while True:
+  print(kite.ltp("WIPRO"))
