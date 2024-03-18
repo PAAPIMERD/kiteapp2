172583cf5b5b7a0baa2584e63a28f33b90a0e382
PAAPIMERD <97976194+PAAPIMERD@users.noreply.github.com>
2024-03-18 22:44:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import time
 
 #all the code required to authentication of kotak neo and kite api should be done here
 
-enc = "nbGuq+7aqzqph05toD7xvOnsf5xPnHIQpPXegCMV2rJSESis5BmlDy0Y325TRJkn3r+2N3Uro1YTTsTomjUVyvJ3UJytpmJo87wEHY+R293hpeczuGQdUg==" #login into kite goto ->inspect->networks->full->enctoken
+enc = "Oiz8IpBuSFK27ck6NRORetnCKbwmSM7RRdIJNd6RrMFwz0PJrqDL52O95HnO4CfhTZc9z+0DEwny5Jfzgc3/CT6IMZGYBLLhEc/smJdq6WnaOGy/HDATeg==" #login into kite goto ->inspect->networks->full->enctoken
 kite = kt.KiteApp("avinash","PQW807",enc)#this block will authenticate the user using enc token in kite(zerodha)
 
 #below you can write code for the authentication of kotakapi (which will be used for order placing purpose)
